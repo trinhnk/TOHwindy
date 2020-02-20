d0d78947b74a575df2bed9e7d61577734efb7a79
tohsofthaupham <tohsofthaupham@gmail.com>
2020-02-20 09:34:52 +0700

    Init php project to cache image, json....

---
+++ application/controllers/Cacheapi.php
@@ -0,0 +1,50 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Cacheapi extends CI_Controller {
+	public function getimage(){
+		$this->load->helper('url');
+		
+		$url = $this->input->get('url');
+		foreach($this->input->get() as $key => $val){
+			if($key != 'url'){
+				$url .= '&'.$key .'=' . $val;
+			}				
+		}
+		
+		$cache_folder = $this->config->item('storage_cache_folder');
+		
+		$last_dot_pos = strrpos($url, ".");
+		$allow_extensions = array('.json', '.png');
+		$file_extension = mb_substr($url, $last_dot_pos);
+		$file_extension = in_array($file_extension, $allow_extensions) ? $file_extension : '.png';
+		
+		$file_path = $cache_folder.md5($url).($file_extension ? $file_extension : '');
+		if(@file_exists($file_path)){
+			$max_time = 3*60*60;
+			$file_time = @filemtime($file_path);
+			$cur_time = strtotime(date('Y-m-d H:i:s'));
+			if($cur_time - $file_time > $max_time){
+				$content = @file_get_contents($url);
+				if($content){
+					@file_put_contents($file_path, $content);
+				}
+			}else{
+				$content = @file_get_contents($file_path);
+			}
+		}else{
+			$content = @file_get_contents($url);
+			if($content){
+				@file_put_contents($file_path, $content);
+			}
+		}
+		
+		if($file_extension == '.json'){
+			header("Access-Control-Allow-Origin: *");
+			die($content);
+		}else{
+			header("Content-type: image/png");
+			die($content);
+		}
+	}
+}
