846ed62b68e1774df849fd3586c9a0b5d12d5ca8
tohsofthaupham <tohsofthaupham@gmail.com>
2020-02-20 10:14:50 +0700

    Add: Cache control to header of request to control cache in browser and cloudflare

--- application/controllers/Cacheapi.php
+++ application/controllers/Cacheapi.php
@@ -20,11 +20,13 @@ class Cacheapi extends CI_Controller {
 		$file_extension = in_array($file_extension, $allow_extensions) ? $file_extension : '.png';
 		
 		$file_path = $cache_folder.md5($url).($file_extension ? $file_extension : '');
+		$max_cache_time = 3*60*60;
 		if(@file_exists($file_path)){
-			$max_time = 3*60*60;
 			$file_time = @filemtime($file_path);
 			$cur_time = strtotime(date('Y-m-d H:i:s'));
-			if($cur_time - $file_time > $max_time){
+			$max_cache_time = $max_cache_time - ($cur_time - $file_time);
+			$max_cache_time = $max_cache_time < 0 ? 0 : $max_cache_time;
+			if($max_cache_time <= 0){
 				$content = @file_get_contents($url);
 				if($content){
 					@file_put_contents($file_path, $content);
@@ -36,9 +38,14 @@ class Cacheapi extends CI_Controller {
 			$content = @file_get_contents($url);
 			if($content){
 				@file_put_contents($file_path, $content);
+			}else{
+				$max_cache_time = 0;
 			}
 		}
 		
+		if($max_cache_time > 0){
+			header("Cache-Control: max-age=".$max_cache_time);
+		}
 		if($file_extension == '.json'){
 			header("Access-Control-Allow-Origin: *");
 			die($content);
