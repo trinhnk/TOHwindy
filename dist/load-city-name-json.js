c4d14d0901ad5c2d78cdecf28cce75e96a03303e
tohsofthaupham <tohsofthaupham@gmail.com>
2020-02-27 09:01:34 +0700

    Fix bug: Check null empty when show json data

--- dist/load-city-name-json.js
+++ dist/load-city-name-json.js
@@ -285,25 +285,27 @@ L.LoadCityNameJSON = L.TileLayer.Ajax.extend({
 				}, 100);
 			}
 		};
-		tile.datum.forEach(function(city){
-			var lng = city[3];
-            var lat = city[4];
-            var cityType = city[2];
-            if( cityType == 'country-1' ){
-                var cityName = L.divIcon({className: 'country-name-1', html: '<div>'+city[1]+'</div>'})
-                markersCity = L.marker([lat, lng],{icon:cityName}).addTo(markerGroup);
-            }else if( cityType == 'country-2' ){
-                var cityName = L.divIcon({className: 'country-name-2', html: '<div>'+city[1]+'</div>'})
-                markersCity = L.marker([lat, lng],{icon:cityName}).addTo(markerGroup);
-            }else{
-				windy_init_timeout(lng,lat, 0, function(interpolatePointLocalResult){
-					if(interpolatePointLocalResult){
-						var cityName = L.divIcon({className: 'city-name '+cityType, html: '<div data-label="'+city[1]+'" data-id="'+lat+'/'+lng+'" data-temp="'+Math.round(interpolatePointLocalResult[2]-273)+'°"><div>'+city[1]+'</div></div>'})
-							markersCity = L.marker([lat, lng],{icon:cityName}).addTo(markerGroup);
-					}
-				});
-            }
-        })
+		if (tile.datum !== null){
+			tile.datum.forEach(function(city){
+				var lng = city[3];
+				var lat = city[4];
+				var cityType = city[2];
+				if( cityType == 'country-1' ){
+					var cityName = L.divIcon({className: 'country-name-1', html: '<div>'+city[1]+'</div>'})
+					markersCity = L.marker([lat, lng],{icon:cityName}).addTo(markerGroup);
+				}else if( cityType == 'country-2' ){
+					var cityName = L.divIcon({className: 'country-name-2', html: '<div>'+city[1]+'</div>'})
+					markersCity = L.marker([lat, lng],{icon:cityName}).addTo(markerGroup);
+				}else{
+					windy_init_timeout(lng,lat, 0, function(interpolatePointLocalResult){
+						if(interpolatePointLocalResult){
+							var cityName = L.divIcon({className: 'city-name '+cityType, html: '<div data-label="'+city[1]+'" data-id="'+lat+'/'+lng+'" data-temp="'+Math.round(interpolatePointLocalResult[2]-273)+'°"><div>'+city[1]+'</div></div>'})
+								markersCity = L.marker([lat, lng],{icon:cityName}).addTo(markerGroup);
+						}
+					});
+				}
+			});
+		}
         L.TileLayer.Ajax.prototype._tileLoaded.apply(this, arguments);
         if (tile.datum === null) { return null; }
         this.addTileData(tile.datum, tilePoint);
