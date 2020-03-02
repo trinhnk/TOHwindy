868e886741515a608a5491dcb91e961ca24419b6
trinhnk <nguyenkimtrinh1993@gmail.com>
2020-03-02 17:20:26 +0700

    Update map color: Cloud, Pressure, Snow

--- demo.js
+++ demo.js
@@ -61,11 +61,11 @@ function initWindyMap(){
         maxZoom: 11,
         minZoom: 3,
     })
-    var Atmospheric_Pressure_Mean = L.tileLayer('http://maps.openweathermap.org/maps/2.0/weather/APM/{z}/{x}/{y}?fill_bound=true&opacity=1&pallette=990:8eb3b8;995:68b4b3;1000:45a7a6;1003:398393;1006:397693;1009:395b93;1015:3a7535;1019:9fa141;1022:ad8839;1025:aa5443;1030:5e3c51&appid='+API_Openweathermap,{
+    var Atmospheric_Pressure_Mean = L.tileLayer('https://b.tile.openweathermap.org/map/pressure/{z}/{x}/{y}?fill_bound=true&opacity=1&pallette=990:8eb3b8;995:68b4b3;1000:45a7a6;1003:398393;1006:397693;1009:395b93;1015:3a7535;1019:9fa141;1022:ad8839;1025:aa5443;1030:5e3c51&appid='+API_Openweathermap,{
         maxZoom: 11,
         minZoom: 3,
     })
-    var Cloudiness = L.tileLayer('http://maps.openweathermap.org/maps/2.0/weather/CL/{z}/{x}/{y}?fill_bound=true&opacity=1&pallette=0:928246;10:847746;30:747474;80:adb7b6;90:bec1c1;100:d5d5cd&appid='+API_Openweathermap,{
+    var Cloudiness = L.tileLayer('https://{s}.sat.owm.io/vane/2.0/weather/CL/{z}/{x}/{y}?appid='+API_Openweathermap+'&fill_bound=true&opacity=1&palette=0:928246;10:847746;30:747474;80:adb7b6;95:bec1c1;100:d5d5cd',{
         maxZoom: 11,
         minZoom: 3,
     })
@@ -82,11 +82,11 @@ function initWindyMap(){
         maxZoom: 11,
         minZoom: 3,
     })
-    var PAS0 = L.tileLayer('http://maps.openweathermap.org/maps/2.0/weather/PAS0/{z}/{x}/{y}?fill_bound=true&opacity=1&appid='+API_Openweathermap,{
+    var PAS0 = L.tileLayer('http://maps.openweathermap.org/maps/2.0/weather/PAS0/{z}/{x}/{y}?fill_bound=true&opacity=1&appid='+API_Openweathermap+'&palette=0:616161;1:455298;10:41a5a7;20:418d41;50:a8a841;80:aa7e3f;120:a74141;200:a841a8',{
         maxZoom: 11,
         minZoom: 3,
     })
-    var SD0 = L.tileLayer('http://maps.openweathermap.org/maps/2.0/weather/SD0/{z}/{x}/{y}?fill_bound=true&opacity=1&appid='+API_Openweathermap,{
+    var SD0 = L.tileLayer('http://maps.openweathermap.org/maps/2.0/weather/SD0/{z}/{x}/{y}?fill_bound=true&opacity=1&appid='+API_Openweathermap+'&palette=0:616161;1:455298;10:41a5a7;20:418d41;50:a8a841;80:aa7e3f;120:a74141;200:a841a8',{
         maxZoom: 11,
         minZoom: 3,
     })
@@ -178,34 +178,14 @@ var handleError = function(err){
 };
 
 var markerGroup = L.layerGroup().addTo(map);
-// var tempMarker = L.layerGroup().addTo(map);
-// setTimeout(function(){ markerGroup = L.layerGroup().addTo(map)}, 3000);
-
-map.on('zoomend',function(e){
+map.on('zoomstart',function(e){
     map.removeLayer(markerGroup);
-    markerGroup = L.layerGroup().addTo(map);
-    // map.removeLayer(tempMarker);
-    // tempMarker = L.layerGroup().addTo(map);
 });
-// map.on('drag', function(e){
-//     // console.log(map.getCenter().lat)
-//     mapCenter = [map.getCenter().lat, map.getCenter().lng]
-// });
-// map._onResize(5); 
-// map.setView(mapCenter, 5);
-// map.addLayer(geojsonTileLayer);
-// map.removeLayer(geojsonTileLayer);
-// map.on('zoomend',function(e){
-//     map.removeLayer(geojsonTileLayer);
-//     geojsonTileLayer = new L.LoadCityNameJSON('https://tiles.windy.com/labels/v1.3/en/{z}/{x}/{y}.json',{
-//             maxZoom: 11,
-//             minZoom: 3,
-//     });
-//     geojsonTileLayer.addTo(map);
-// });
+map.on('zoomend',function(e){
+    markerGroup = L.layerGroup().addTo(map);
+});
 
 map.on('drag', function(e){
-    // console.log(map.getCenter().lat)
     mapCenter = [map.getCenter().lat, map.getCenter().lng]
 });
 
