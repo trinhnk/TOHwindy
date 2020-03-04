67ed47dac4c6d819a755f4f36849955a814ba7ca
tohsofthaupham <tohsofthaupham@gmail.com>
2020-03-04 14:09:33 +0700

    Fix bug: Dont show map because incorrect position of stylesheet file

--- index2.php
+++ index2.php
@@ -5,12 +5,48 @@
 	<meta charset="utf-8">
 </head>
 <body>
-	<?php $resource_url = 'http://resource.goweatherradar.com/';?>
-	<?php //$resource_url = 'http://mylocal.com/tohwindy/';?>
-	
 	<div id="map"></div>
 	<div id="temp"></div>
+
+	<?php $resource_url = 'http://resource.goweatherradar.com/';?>
+	<?php $resource_url = 'http://mylocal.com/tohwindy/';?>
+	<?php 
+		function recurse_copy($src,$dst) { 
+			$dir = opendir($src); 
+			@mkdir($dst); 
+			while(false !== ( $file = readdir($dir)) ) { 
+				if (( $file != '.' ) && ( $file != '..' )) { 
+					if ( is_dir($src . '/' . $file) ) { 
+						recurse_copy($src . '/' . $file,$dst . '/' . $file); 
+					} 
+					else { 
+						copy($src . '/' . $file,$dst . '/' . $file); 
+					} 
+				} 
+			} 
+			closedir($dir); 
+		} 
+	?>
+	<?php 
+		define('FCPATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
+		
+		$version = file_get_contents(FCPATH.'version.txt');
+		copy (FCPATH.'my_css.css', FCPATH.'assets/'.$version.'_my_css.css');
+		copy (FCPATH.'style.css', FCPATH.'assets/'.$version.'_style.css');
+		copy (FCPATH.'jscript.js', FCPATH.'assets/'.$version.'_jscript.js');
+		copy (FCPATH.'dist/load-city-name-json.js', FCPATH.'assets/'.$version.'_load-city-name-json.js');
+
+		$source_fontawesome = FCPATH.'dist/fontawesome-free-5.12.1';
+		$des_fontawesome = FCPATH.'assets/fontawesome_'.$version;
+		if(!@file_exists($des_fontawesome)){
+			recurse_copy($source_fontawesome, $des_fontawesome);
+		}
+	?>
 	
+	<link rel="stylesheet" href="<?php echo $resource_url;?>assets/<?php echo $version;?>_style.css" />
+	<link rel="stylesheet" href="<?php echo $resource_url;?>assets/fontawesome_<?php echo $version;?>/css/all.css"/>
+	<link rel="stylesheet" href="<?php echo $resource_url;?>assets/<?php echo $version;?>_my_css.css" />
+
 	<script src="<?php echo $resource_url;?>dist/jquery/1.9.1/jquery.min.js"></script>
 
 	<!-- Leaflet 0.7.7 -->
@@ -20,26 +56,10 @@
 	<!--wind-js-leaflet-->
 	<link rel="stylesheet" href="<?php echo $resource_url;?>dist/wind-js-leaflet.css" />
 	<script src="<?php echo $resource_url;?>dist/wind-js-leaflet.js"></script>
-
-	<!-- Leaflet TileLayer GeoJSON -->
-	<script src="<?php echo $resource_url;?>dist/load-city-name-json.js"></script>
-
-	<!-- CSS Thêm Mới -->
-	<!-- <script src="<?php echo $resource_url;?>/dist/font/kit.fontawesome.js"></script> -->
-	<!-- <script src="<?php echo $resource_url;?>/dist/jquery/3.4.1/jquery.min.js"></script> -->
-	<link rel="stylesheet" href="<?php echo $resource_url;?>/dist/fontawesome-free-5.12.1/css/all.css"/>
 	
-	<?php 
-		define('FCPATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
-		
-		$version = file_get_contents(FCPATH.'version.txt');
-		copy (FCPATH.'my_css.css', FCPATH.'assets/'.$version.'_my_css.css');
-		copy (FCPATH.'style.css', FCPATH.'assets/'.$version.'_style.css');
-		copy (FCPATH.'jscript.js', FCPATH.'assets/'.$version.'_jscript.js');
-	?>
-	<link rel="stylesheet" href="<?php echo $resource_url;?>assets/<?php echo $version;?>_style.css" />
+	<!-- Leaflet TileLayer GeoJSON -->
+	<script src="<?php echo $resource_url;?>assets/<?php echo $version;?>_load-city-name-json.js"></script>
 	<script src="<?php echo $resource_url;?>assets/<?php echo $version;?>_jscript.js"></script>
-	<link rel="stylesheet" href="<?php echo $resource_url;?>assets/<?php echo $version;?>_my_css.css" />
 	
 	<script>
 		function check_checkbox(callback){
